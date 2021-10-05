e8e843b243652670e7ef960b59b75043e7e51ee5
Drip <91963513+Fripoff@users.noreply.github.com>
2021-10-05 22:15:44 +0300

    Create script.lua
    Blox fruits main script

---
+++ script.lua
@@ -0,0 +1,10 @@
+--[[
+    Project KS was made by: Trix#2624
+    Visit the Youtube channel at: https://www.youtube.com/channel/UCk3cs2_4ky3fLIeOae02skg
+]]
+
+local test = true;
+
+if test then
+  print("Working")
+end
